44af1f340dc8c91a7e16b88cf979b47bb8d781dd
ek2dev_imac <turokg@yahoo.com>
2019-04-07 13:07:46 +0700

    alive notification

--- vk_notifier.py
+++ vk_notifier.py
@@ -2,32 +2,43 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import random
 import json
+import time
+import datetime
 
 from threading import Thread
 
 
 class VKNotifier(Thread):
-    def __init__(self):
+    def __init__(self, name='unknown vk_notifier', sleep_time = 3600):
         Thread.__init__(self)
         try:
             with open('auth.json', 'r') as f:
                 self.auth = json.loads(" ".join(f.readlines()))
-        except Exception:
+        except Exception as e:
             raise Exception('invalid auth data')
+        self.name = name
+        self.sleep_time = sleep_time
         self.vk_session_ = vk_api.VkApi(token=self.auth['token_debug'])
         self.vk_debug = self.vk_session_.get_api()
-
+        self.start_date = datetime.datetime.now()
         self.vk_session = vk_api.VkApi(token=self.auth['token_notifier'])
         self.vk_notifier = self.vk_session.get_api()
+        self.alive_notifier = Thread(target=self.alive_notification(), args=[self.name])
+        self.alive_notifier.start()
 
-    def alarm(self, message='unknown alarm'):
+    def alive_notification(self):
+        while True:
+            self.log(f"{self.name}'s up for {str(datetime.datetime.now() - self.start_date).split('.', 2)[0]}")
+            time.sleep(self.sleep_time)
+
+    def alarm(self, message):
         try:
             self.vk_notifier.messages.send(user_id=self.auth['user_id'],
                                            message=message, random_id=random.randint(0, 9223372036854775808))
         except Exception as e:
             self.log(e.__str__())
 
-    def debug_message(self, message='debug message'):
+    def debug_message(self, message):
         try:
             self.vk_debug.messages.send(user_id=self.auth['user_id'],
                                         message=message, random_id=random.randint(0, 9223372036854775808))
